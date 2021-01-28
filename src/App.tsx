342ca3adc9cca83b76463932acbaa8ff52c9d7b5
n7231 <itaniaanni@gmail.com>
2021-01-28 21:37:02 +0200

    done

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Counter } from "./counter";
+
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Counter>
+        {({ count, setCount }) => (
+          <div>
+            {count}
+            <button onClick={() => setCount(count + 1)}>+</button>
+          </div>
+        )}
+      </Counter>
+    </div>
+  );
+};
+
+export default App;
